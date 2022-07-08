2f793db7558fe61eb0cf150835e7a8d3c45fbcbb
Laura R. Contreras <laura.88.r.c@gmail.com>
2022-07-07 21:02:17 -0300

    parciales 2018 1, 2 y 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
+    let precio;
+    let porcentaje;
+    let precioFinal;
 
+    precio=prompt("Ingrese precio");
+    precio=parseInt(precio);
+    porcentaje=prompt("Ingrese porcentaje de descuento");
+    porcentaje=parseInt(porcentaje);
+
+    precioFinal=precio-(precio*porcentaje/100);
+    document.getElementById("elPrecioFinal").value=precioFinal;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+    let largo;
+    let ancho;
+    let perimetro;
 
+    largo=prompt("Ingrese el largo");
+    largo=parseInt(largo);
+    ancho=prompt("Ingrese el ancho");
+    ancho=parseInt(ancho);
+
+    perimetro=2*largo+2*ancho;
+
+    alert("El perímetro del rectángulo es de: "+perimetro)
 }
